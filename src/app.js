e3e902c1288c6fb52c867f7fc51b2b332f2981e7
Víctor Company Bernal <vcompanyb@vulturdev.com>
2021-08-02 19:50:07 +0200

    Cambios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,53 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("myButton").addEventListener("click", function() {
+    const domainNames = generateDomainNames();
+
+    appendDomainNames(
+      document.querySelector(".domains_container"),
+      domainNames,
+      "p"
+    );
+
+    appendDomainNames(
+      document.querySelector(".another_domains_container"),
+      domainNames,
+      "li"
+    );
+  });
+
+  appendDomainNames(
+    document.querySelector(".domains_container"),
+    generateDomainNames(),
+    "p"
+  );
 };
+
+function generateDomainNames() {
+  let domainNames = [];
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronouns.length; i++) {
+    let pronoun = pronouns[i];
+
+    for (let j = 0; j < adjs.length; j++) {
+      let adj = adjs[j];
+
+      for (let z = 0; z < nouns.length; z++) {
+        let noun = nouns[z];
+
+        domainNames.push(`${pronoun}${adj}${noun}.com`);
+      }
+    }
+  }
+  return domainNames;
+}
+
+function appendDomainNames(element, domainNames, tag) {
+  console.log(domainNames.join(`</${tag}><${tag}>`));
+  element.innerHTML =
+    `<${tag}>` + domainNames.join(`</${tag}><${tag}>`) + `</${tag}>`;
+}
